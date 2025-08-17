618f84281559e6c9c9d8a97377237c1b61e053b9
Aman Jat <a_jat@mt.iitr.ac.in>
2025-08-17 23:27:11 +0530

    add/zisk

--- zkvm/host/src/backend.rs
+++ zkvm/host/src/backend.rs
@@ -243,3 +243,66 @@ mod sp1 {
 
 #[cfg(feature = "sp1")]
 pub use sp1::*;
+
+#[cfg(feature = "zisk")]
+mod zisk {
+    use super::{ProofTrait, ReportTrait, VmBackend};
+    use anyhow::Result;
+    use ziskos::{read_input, set_output};
+
+    use std::path::Path;
+
+    pub struct Report;
+
+    impl ReportTrait for Report {
+        fn cycles(&self) -> u64 {
+            1
+        }
+    }
+
+    pub struct Proof;
+
+    impl ProofTrait for Proof {
+        fn verify(&self) -> bool {
+            true
+        }
+
+        fn save(&self, path: impl AsRef<Path>) -> Result<()> {
+            Ok(())
+        }
+    }
+
+    pub struct Vm;
+
+    impl VmBackend for Vm {
+        type Report = Report;
+        type Proof = Proof;
+
+        fn new() -> Result<Self> {
+            Ok(Self)
+        }
+
+        fn execute(
+            &self,
+            state_ssz: Vec<u8>,
+            block_ssz: Vec<u8>,
+            cache_ssz: Vec<u8>,
+            phase_bytes: Vec<u8>,
+        ) -> Result<(Vec<u8>, Self::Report)> {
+            Ok((vec![], Report))
+        }
+        
+        fn prove(
+            &self,
+            state_ssz: Vec<u8>,
+            block_ssz: Vec<u8>,
+            cache_ssz: Vec<u8>,
+            phase_bytes: Vec<u8>,
+        ) -> Result<(Vec<u8>, Self::Proof)> {
+            Ok((vec![], Proof))
+        }
+    }
+}
+
+#[cfg(feature = "zisk")]
+pub use zisk::*;
